5ad5f1ca01f8a44c208161ee0c6b600bddf45afb
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2021-11-25 11:32:00 +0600

    add hooks

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
+    const [products]= useProducts();
     return (
         <div>
+            <h3>{products.length}</h3>
             <h2>This is Order Review</h2>
         </div>
     );
---
+++ src/hooks/useProducts.js
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useProducts=()=>{
+    const[products,setProducts]= useState([]);
+    useEffect(()=>{
+        fetch('./products.JSON')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
+    //return necessary things
+    return [products] ;
+}
+
+export default useProducts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const useProducts = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+
+// export default useProducts;
