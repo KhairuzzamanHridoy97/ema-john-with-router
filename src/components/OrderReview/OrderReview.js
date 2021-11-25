105d0e1188a81faab9776020f5331e2103f94016
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2021-11-26 01:49:20 +0600

    added cart

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 
 const OrderReview = () => {
-    const [products]= useProducts();
+    const [products,setProducts]= useProducts();
+    const [cart]=useCart(products)
     return (
         <div>
-            <h3>{products.length}</h3>
-            <h2>This is Order Review</h2>
+
+        <div className='product-container'>
+            
+        </div>
+        <div>
+            <Cart cart={cart}></Cart>
+        </div>
         </div>
     );
 };
 
 export default OrderReview;
+
+/* 
+   <h3>{products.length}</h3>
+            <h2>{cart.length} </h2>
+            <h2>This is Order Review</h2>
+            <Cart cart={cart}></Cart>
+*/
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,20 +1,17 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const useProducts=()=>{
-    const[products,setProducts]= useState([]);
+const useProducts =()=>{
+    const [products,setProducts]=useState([]);
     useEffect(()=>{
         fetch('./products.JSON')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
     //return necessary things
-    return [products] ;
+    return [products,setProducts];
 }
 
 export default useProducts;
-
-
 
 
 
